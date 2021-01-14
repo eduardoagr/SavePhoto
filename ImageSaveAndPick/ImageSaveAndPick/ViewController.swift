9fed6954a4ea088af9b9845e05214207750ea83f
eduardoagr <egomezr@outlook.com>
2021-01-14 14:32:28 +0100

    TakePhoto

---
+++ ImageSaveAndPick/ImageSaveAndPick/ViewController.swift
@@ -0,0 +1,51 @@
+//
+//  ViewController.swift
+//  ImageSaveAndPick
+//
+//  Created by user183363 on 1/14/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+
+    @IBOutlet weak var imageTaken: UIImageView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    @IBAction func saveImage(_ sender: Any) {
+        let imageData = imageTaken.image!.pngData()
+        let compressedImage = UIImage(data: imageData!)
+        UIImageWriteToSavedPhotosAlbum(compressedImage!, nil, nil, nil)
+        
+        let alert = UIAlertController(title: "Saved", message: "Your picture has been saved", preferredStyle: .alert)
+        
+        let okAction = UIAlertAction(title: "OK", style: .default, handler: nil)
+        
+        alert.addAction(okAction)
+        
+        self.present(alert, animated: true, completion: nil)
+        
+    }
+    @IBAction func openCamera(_ sender: UIBarButtonItem) {
+        
+        if UIImagePickerController.isSourceTypeAvailable(UIImagePickerController.SourceType.camera) {
+            let imagePicker = UIImagePickerController()
+            imagePicker.delegate = self
+            imagePicker.sourceType = UIImagePickerController.SourceType.camera
+            imagePicker.allowsEditing = false
+            self.present(imagePicker, animated: true, completion: nil)
+        }
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        if let pickedImage = info[UIImagePickerController.InfoKey.originalImage] as? UIImage {
+            
+            imageTaken.contentMode = .scaleToFill
+            imageTaken.image = pickedImage
+        }
+        picker.dismiss(animated: true, completion: nil)
+    }
+}
+
